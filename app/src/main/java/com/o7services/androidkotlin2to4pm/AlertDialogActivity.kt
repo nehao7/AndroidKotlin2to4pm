d805f5a1605ba5fa87fee0cade27d924efd5fa2e
Neha <neha@o7tec.com>
2025-01-24 15:56:03 +0530

    AlertDialog , Implicit

---
+++ app/src/main/java/com/o7services/androidkotlin2to4pm/AlertDialogActivity.kt
@@ -0,0 +1,137 @@
+package com.o7services.androidkotlin2to4pm
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.o7services.androidkotlin_9_11am.databinding.ActivityAlertDialogBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class AlertDialogActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAlertDialogBinding
+    var list = arrayOf("one", "two", "three", "four")
+    var checklist = booleanArrayOf(true, false, true, true)
+    var simpleDateFormat = SimpleDateFormat("dd/MMM/yyyy", Locale.getDefault())
+    var timeFormat = SimpleDateFormat("HH:mm:ss a", Locale.getDefault())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityAlertDialogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "fcghjk", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.button.setOnClickListener {
+            Snackbar.make(it, "This is a simple Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Undo", {
+                    Toast.makeText(
+                        this, "Toast is invoked from snackbar",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }).show()
+        }
+
+        binding.btnDialog.setOnClickListener {
+           AlertDialog.Builder(this) .apply {
+                setTitle("This is Alert Dialog")
+               setMessage("This is my Message")
+           setNegativeButton("No") { _, _ ->
+                }
+            setPositiveButton("yes") { _, _ -> }
+               setNeutralButton("Cancel"){_,_->}
+            setCancelable(true)
+          show()
+            }
+        }
+
+
+        binding.btnSingleChoice.setOnClickListener {
+            AlertDialog.Builder(this).apply {
+                setTitle("My List")
+                setItems(list) { _, which ->
+                    Toast.makeText(
+                        this@AlertDialogActivity,
+                        list[which].toString(), Toast.LENGTH_LONG
+                    ).show()
+                }
+                setNegativeButton("No") { _, _ ->
+                }
+                setPositiveButton("yes") { _, _ -> }
+                setCancelable(false)
+                show()
+
+            }
+        }
+
+        binding.btnMultipleChoice.setOnClickListener {
+            AlertDialog.Builder(this).apply {
+                setTitle("This is Alert Dialog")
+                setMultiChoiceItems(list,checklist){_,which,isChecked->
+                    checklist.set(which,isChecked)
+                    Toast.makeText(
+                        this@AlertDialogActivity,
+                        list[which].toString(), Toast.LENGTH_LONG
+                    ).show()
+
+                }
+                setNegativeButton("No") { _, _ ->
+                }
+                setPositiveButton("yes") { _, _ -> }
+                setCancelable(true)
+                show()
+            }
+        }
+
+        binding.btnCustomDialog.setOnClickListener {
+            var dialog = Dialog(this)
+            dialog.setContentView(R.layout.custom_layout)
+//            var btn=dialog.findViewById<Button>(R.id.btnCancel)
+
+            dialog.window?.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+            dialog.show()
+            dialog.findViewById<Button>(R.id.btnCancel).setOnClickListener{
+                dialog.dismiss()
+            }
+        }
+
+        binding.btnDatePicker.setOnClickListener {
+
+            DatePickerDialog(
+                this,
+                { _, year, month, dateOfMonth ->
+                    Log.e("date", "picked Date is $year $month $dateOfMonth")
+                    val calendar = Calendar.getInstance()
+                    calendar.set(year, month, dateOfMonth)
+                    var formattedDate = simpleDateFormat.format(calendar.time)
+                    binding.btnDatePicker.setText(formattedDate)
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DATE),
+            ).show()
+        }
+
+    }
+}
--- app/src/main/java/com/o7services/androidkotlin2to4pm/MainActivity.kt
+++ app/src/main/java/com/o7services/androidkotlin2to4pm/MainActivity.kt
@@ -9,29 +9,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.o7services.androidkotlin2to4pm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     var button : Button?=null
     var editText:EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         Toast.makeText(this, "Oncreate", Toast.LENGTH_SHORT).show()
-        button=findViewById(R.id.btnClick)
+//        button=findViewById(R.id.btnClick)
         editText=findViewById(R.id.edt1)
-        button?.setOnClickListener {
+        binding.btnClick.setOnClickListener {
             var intent=Intent(this,Activity2::class.java)
             var mydata=editText?.text.toString()
             intent.putExtra("data",mydata)
             startActivity(intent)
             Toast.makeText(this,editText?.text.toString(),Toast.LENGTH_SHORT).show()
         }
+         binding.btnImplicit.setOnClickListener {
+            var intent=Intent(this,ImplicitActivity::class.java)
+
+            startActivity(intent)
+        }
+         binding.btnAlertDialog.setOnClickListener {
+            var intent=Intent(this,AlertDialogActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     override fun onStart() {
